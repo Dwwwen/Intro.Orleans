2fd98e0db2fc83c672996b3e7cce56b698ce12d9
wendi <wendi@skieer.com>
2019-03-13 09:03:59 +0800

    初始化

---
+++ Code/Intro.Orleans.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Intro.Orleans.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Code/Intro.Orleans.Silo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Intro.Orleans.Silo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
