127c5dcf3d6ad1e9352e070449b4aa964b7647f6
wendi <wendi@skieer.com>
2019-03-20 09:03:00 +0800

    初始化Silo

--- Code/Intro.Orleans.Silo/Program.cs
+++ Code/Intro.Orleans.Silo/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Intro.OrleansBasics.Grains;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Configuration;
+using Orleans.Hosting;
+using System;
+using System.Threading.Tasks;
 
 namespace Intro.OrleansBasics.Silo
 {
@@ -7,6 +13,41 @@ namespace Intro.OrleansBasics.Silo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        private static async Task<int> RunMainAsync()
+        {
+            try
+            {
+                var host = await StartSilo();
+                Console.WriteLine("Press Enter to terminate silo...");
+                Console.ReadLine();
+                await host.StopAsync();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return 1;
+            }
+        }
+
+        private static async Task<ISiloHost> StartSilo()
+        {
+            var builder = new SiloHostBuilder()
+                .UseLocalhostClustering()
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = "develop";
+                    options.ServiceId = "OrleansBasics";
+                })
+                .ConfigureApplicationParts(parts =>
+                    parts.AddApplicationPart(typeof(HelloGrain).Assembly).WithReferences())
+                .ConfigureLogging(logging => logging.AddConsole());
+
+            var host = builder.Build();
+            await host.StartAsync();
+            return host;
         }
     }
 }
